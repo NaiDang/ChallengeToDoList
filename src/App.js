057958450dac6751ffd3bee7b0f86e95a3877f1d
NaiDang <Naid4243@gmail.com>
2023-08-14 10:18:35 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "./App.css";
 import NavComponent from './components/NavComponent';
 import HomeComponent from './components/HomeComponent';
